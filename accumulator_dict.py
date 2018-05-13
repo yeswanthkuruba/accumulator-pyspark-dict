dd7df5f413f8d4450071411a5764eeef38b983f8
yeswanthkuruba <39231357+yeswanthkuruba@users.noreply.github.com>
2018-05-13 12:57:11 +0530

    Update accumulator_dict.py

--- accumulator_dict.py
+++ accumulator_dict.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext  
 from pyspark import AccumulatorParam
+from pyspark.sql import HiveContext
 
 class DictAccumulatorParam(AccumulatorParam):
     def zero(self,  value):
@@ -13,22 +14,33 @@ class DictAccumulatorParam(AccumulatorParam):
                 acc1[i].add(val)
         return acc1
 
-
 def accumDict(line):
     global dict1
-    categorical_columns = ['a','b','c'] # column names can be given as per requirement
+    categorical_columns = b_categorical_columns.value
     temp_dict = {}
     for i in range(len(categorical_columns)):
         temp_dict[categorical_columns[i]]=set([line[i]])
     dict1 += temp_dict
-
     
+def distinctValues():
+    sc = SparkContext()
+    sqlContext = HiveContext(sc)
+    data = sqlContext.sql("Select * from "+table_name)
+    categorical_columns = ['a','b','c'] # column names can be given as per requirement
+    global b_categorical_columns
+    global dict1
+    default_dict = {}
+    for i in categorical_columns:
+        default_dict[i] = set()
+
+    dict1 = sc.accumulator(default_dict,DictAccumulatorParam())
+    b_categorical_columns = sc.broadcast(categorical_columns)
+    data_ = data[categorical_columns]
+    data_.rdd.foreach(accumDict)
+    cat_distinct_dict = dict1.value
+    return cat_distinct_dict
+
+if __name__ == "__main__":
+    cat_distinct_dict = distinctValues()
+    print(cat_distinct_dict)
     
-default_dict = {}
-for i in categorical_columns:
-    default_dict[i] = set()
-
-global dict1
-dict1 = sc.accumulator(default_dict,DictAccumulatorParam())
-
-print(dict1)
