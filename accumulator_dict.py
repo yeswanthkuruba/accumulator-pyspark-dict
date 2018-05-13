a7b8bd64c70cb5af6a472339a86256c3cfc40af2
yeswanthkuruba <39231357+yeswanthkuruba@users.noreply.github.com>
2018-05-13 11:20:55 +0530

    Create accumulator_dict.py

---
+++ accumulator_dict.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext  
+from pyspark import AccumulatorParam
+
+class DictAccumulatorParam(AccumulatorParam):
+    def zero(self,  value):
+        dict_={}
+        for i in value.keys():
+            dict_[i] = set()
+        return dict_
+    def addInPlace(self, acc1, acc2):
+        for i in acc1.keys():
+            for val in acc2[i]:
+                acc1[i].add(val)
+        return acc1
+
+
+def accumDict(line):
+    global dict1
+    categorical_columns = ['a','b','c'] # column names can be given as per requirement
+    temp_dict = {}
+    for i in range(len(categorical_columns)):
+        temp_dict[categorical_columns[i]]=set([line[i]])
+    dict1 += temp_dict
